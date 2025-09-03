629c6269fb8054e9b4b4d2639926ffb2607e97ae
beatrizvidalb <anabeatrizvidalbezerra@gmail.com>
2025-09-03 11:52:12 -0300

    Add files via upload

---
+++ provaarea.py
@@ -0,0 +1,6 @@
+base = int(input("Por favor digite a base do retângulo: "))
+altura = int(input("Por favor digite a altura do retângulo: "))
+
+area = base * altura
+
+print("A área do retângulo digitado é:",area)
